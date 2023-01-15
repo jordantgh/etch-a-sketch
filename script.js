7ff399bb6781356ef98a95e3bb715e87e97d6a36
s1349795 <s1349795@ed.ac.uk>
2023-01-15 14:44:43 +0000

    Add buttons to set size and refresh grid

--- script.js
+++ script.js
@@ -1,10 +1,31 @@
 let container = document.querySelector(".container");
 
-for (var i = 0; i < 256; i++) {
-    let gridSquare = document.createElement('div');
-    gridSquare.className = "grid-square";
-    container.append(gridSquare);
-    gridSquare.addEventListener('mouseover', () => {
-        gridSquare.style.backgroundColor = "#6ebb95";
-    })
+let root = document.querySelector(":root");
+
+function game() {
+    let h = getComputedStyle(root).getPropertyValue("--h");
+    for (var i = 0; i < (h*h); i++) {
+        let gridSquare = document.createElement('div');
+        gridSquare.className = "grid-square";
+        container.append(gridSquare);
+        gridSquare.addEventListener('mouseover', () => {
+            gridSquare.style.backgroundColor = "#232524";
+        })
+    }
 }
+
+let sizeAlert = document.querySelector("#set-size");
+sizeAlert.addEventListener('click', () => {
+    let h = prompt("Enter number of cells per side: ");
+    root.style.setProperty("--h", h);
+    document.querySelectorAll(".grid-square").forEach(s => s.remove());
+    game();
+})
+
+let refreshGrid = document.querySelector("#refresh");
+refreshGrid.addEventListener('click', () => {
+    document.querySelectorAll(".grid-square").forEach(s => s.remove());
+    game();
+})
+
+game();
